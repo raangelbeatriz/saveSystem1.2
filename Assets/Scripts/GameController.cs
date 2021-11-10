e23be826cc21784f5e56ea4a7c52f481385c106f
Beatriz Rangel <raangelbeatriz@gmail.com>
2021-11-10 16:00:02 -0300

    add delete useless stuff

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -21,6 +21,18 @@ public class GameController : MonoBehaviour
     }
     void Start()
     {
+        initializeGame();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        coinText.text = coin.ToString();
+        lifeText.text = life.ToString();
+    }
+    private void initializeGame()
+    {
         if (SaveManager.sv.hasLoad == true)
         {
             life = SaveManager.sv.activeSave.life;
@@ -31,7 +43,7 @@ public class GameController : MonoBehaviour
 
             coinsList = SaveManager.sv.activeSave.coinsList;
 
-            for(int i = 0; i < coinsList.Count; i++)
+            for (int i = 0; i < coinsList.Count; i++)
             {
                 if (coinsList[i].active == false)
                 {
@@ -40,22 +52,10 @@ public class GameController : MonoBehaviour
 
             }
         }
-
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        coinText.text = coin.ToString();
-        lifeText.text = life.ToString();
-    }
-
     public void playerHit()
     {
-        Player.player.transform.position = respawnPoint;
         life -= 1;
-        //SaveManager.sv.activeSave.life = life;
-        //SaveManager.sv.Save();
     }
 
 
@@ -66,16 +66,11 @@ public class GameController : MonoBehaviour
         coinClass.id = item.id;
         coinClass.active = item.active;
         coinsList.Add(coinClass);
-        //SaveManager.sv.activeSave.coin = coin;
-        //SaveManager.sv.activeSave.coinsList = coinsList;
-        //SaveManager.sv.Save();
     }
 
     public void setRespawnPoint(Vector3 position)
     {
         respawnPoint = position;
-        //SaveManager.sv.activeSave.respawnPoint = respawnPoint;
-        //SaveManager.sv.Save();
         print(transform.position);
         saveState();
     }
